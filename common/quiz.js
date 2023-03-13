5e59dff3f7974e09c45200a9c6284406fd6765f9
khyeonj <99318531+khyeonj@users.noreply.github.com>
2023-03-13 16:26:40 +0900

    quiz

---
+++ common/quiz.js
@@ -0,0 +1,97 @@
+let main = document.querySelector(".main");
+
+let startBtn = document.querySelector(".start_btn");
+let resultBtn = document.querySelector(".result");
+let nextBtn = document.querySelectorAll(".next");
+let retryBtn = document.querySelector(".retryAll");
+let confirmBtn = document.querySelector('.confirm');
+
+let startPage = document.querySelector("section[data-page='start']");
+let quizPage = document.querySelector("section[data-page='quiz']");
+let resultPage = document.querySelector("section[data-page='result']");
+
+let quiz = document.querySelector(".quiz");
+console.log(quiz);
+
+//총 문제 수
+// let totalQnum = document.querySelector(".totalQnum");
+// totalQnum.innerHTML = quiz.length;
+// 맞춘 문제 수 (true 갯수 체크해서 띄우기)
+
+
+//start클릭시 quiz페이지 block
+startBtn.addEventListener("click", function () {
+  startPage.style.display = "none";
+  quizPage.style.display = "block";
+});
+
+//result클릭시 result페이지 
+resultBtn.addEventListener("click", function () {
+  quizPage.style.display = "none";
+  resultPage.style.display = "block";
+});
+
+
+//내가 선택한 답 체크
+let buttons = document.querySelectorAll('.select');
+
+buttons.forEach(button => {
+  button.addEventListener('click', function () {
+    buttons.forEach(b => {
+      if (b !== button) b.classList.remove('checked');
+    });
+    this.classList.toggle('checked');
+  });
+});
+
+//정답체크 
+//내가 선택한 답 = 정답이면 .true, 아니면 .false
+let quizData = JSON.parse(quiz.dataset.quiz);
+let mTryNum = quizData.attempts;
+let usetTryNum = 1;
+console.log(quizData);
+
+confirmBtn.addEventListener("click", function(){
+  let checkedAns = document.querySelector('.checked')
+
+  //정답 선택하지 않았을 때
+  if(!checkedAns){
+    return;
+  }
+
+  let choice = checkedAns.dataset.choice;
+  let coAns = document.querySelector(`button[data-choice="${quizData.answer}"]`);
+  let checkBtn = document.querySelector('.checked');
+
+  console.log(usetTryNum);
+  console.log(mTryNum);
+  console.log(choice);
+
+  let quizElement = confirmBtn.closest('.quiz');
+
+  if(choice == quizData.answer){//정답일때-------------
+    quizElement.classList.remove('false');
+    quizElement.classList.add('true');
+    quiz.classList.add('finished');
+    confirmBtn.style.display = 'none';
+    //정답에 true 추가
+    coAns.classList.add('true');
+    //모달창 - 정답입니다.
+
+  }else if(choice !== quizData.answer){//정답 틀렸을 때-------------
+    usetTryNum++;
+    checkBtn.classList.remove('checked');
+    if(usetTryNum == mTryNum){
+      //사용자 시도횟수 없는 경우
+      quizElement.classList.add('false');
+      confirmBtn.addEventListener('click', function () {
+        quiz.classList.add('finished');
+        confirmBtn.style.display = 'none';
+        //정답에 true 추가
+        coAns.classList.add('true');
+      });
+    }
+  }
+
+    
+});
